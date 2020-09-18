cb943cf9d9d19ac23bedf76e3bb910ebc8f30bc0
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-18 13:48:30 +0800

    Add files via upload

---
+++ 314.java
@@ -0,0 +1,24 @@
+class  Hello
+{
+	public static void main(String[] args) 
+	{int x=2015,m=10,q=8;
+	if (m==1)
+		{System.out.println("m=13");
+		}
+	else if (m==2)
+
+		{System.out.println("m=14");
+		}
+		
+		{int h=(q+26*(m+1)/10+x+x/4+5*(x/100))%7;
+		
+
+		
+		System.out.println("h="+h);
+		}
+	}	
+}
+	
+	
+	
+	
