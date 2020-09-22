291a71b8b2cc83c81ae95da73d1b35998b82b7be
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-22 22:49:18 +0800

    Update 2-6.java

--- 2-6.java
+++ 2-6.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter a number between 0 and 1000");
-		int  number  = input.nextInt();
-		int  a = number%10;
-		int  b = number/100;
-		int  c = number/10;
-		int  d = c%10;
-		int  e = a+b+d;
-		System.out.print("The sum of the digits is"+e);
+		int number = input.nextInt();
+		int a = number % 10;
+		int b = number / 100;
+		int c = number / 10;
+		int d = c % 10;
+		int e = a + b + d;
+		System.out.print(" The sum of the digits is " + e);
 	}
  }
