5df3ebcbc140a7d6d59fed028e8b653c487bb1d2
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-22 11:27:16 +0800

    Add files via upload

---
+++ 2-9.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+	public class Acceleration {
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter v0,v1,and t:");
+			double v0 = input.nextDouble();
+			double v1 = input.nextDouble();
+			double t = input.nextDouble();
+			double averageAcceleration = ( v1 - v0 )/t;
+			System.out.println("The average acceleration is"+averageAcceleration);
+		}
+
+	}
+
