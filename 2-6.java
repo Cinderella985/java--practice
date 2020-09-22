c8a1b74e3ab367b09cbf42a4f904e6b828d3dcf9
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-22 23:20:58 +0800

    Update 2-6.java

--- 2-6.java
+++ 2-6.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
  class Number {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter a number between 0 and 1000");
+		System.out.print("Enter a number between 0 and 1000" );
 		int number = input.nextInt();
 		int a = number % 10;
 		int b = number / 100;
