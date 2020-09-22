c5ed2738ad48e724be37141590ad0c9ab4a56275
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-22 23:05:09 +0800

    Update 2-8.java

--- 2-8.java
+++ 2-8.java
@@ -1,28 +1,18 @@
 import java.util.Scanner;
+
 public class Gmt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the time zone offset to GMT:");
 		long timeZoneOffset = input.nextLong();
-		long totalMilliseconds = System . currentTimeMillis();
-		long totalSeconds = totalMilliseconds/1000;
-		long totalMinutes = totalSeconds/60;
-		long totalHours = totalMinutes/60;
+		long totalMilliseconds = System.currentTimeMillis();
+		long totalSeconds = totalMilliseconds / 1000;
+		long totalMinutes = totalSeconds / 60;
+		long totalHours = totalMinutes / 60;
 		long currentSecond = totalSeconds % 60;
 		long currentMinutes = totalMinutes % 60;
-		long currentHours = (timeZoneOffset +totalHours)%24;
-		System.out.println("The current time is" +currentHours+":"+currentMinutes+":"+currentSecond)
-		
-	;
-	
-	
+		long currentHours = ( timeZoneOffset + totalHours ) % 24;
+		System.out.println("The current time is" + currentHours + ":" + currentMinutes + ":" + currentSecond);
 	}
-	
-	
-	
-	
-	
-	
-
 }
