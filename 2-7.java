67288ba7ee4bb900b60667d44cfa928a03423532
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-22 10:30:31 +0800

    Add files via upload

---
+++ 2-7.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Minutes {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter the number of minutes:");
+		long minutes = input.nextInt();
+		long years = minutes / 365;
+		long days = minutes % 365;
+		System.out.println(minutes + "minutes is approximately"+ years +"and"+ days );
+	}
+
+}
