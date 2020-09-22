f629fbe4b46c0414ac99f9bebf2a20e0805fb697
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-22 23:19:09 +0800

    Update 2-4.java

--- 2-4.java
+++ 2-4.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
  class Pounds {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Enter a number in pounds");
+		System.out.print("Enter a number in pounds" );
 	double pounds = input.nextDouble();
 	double kilograms = pounds * 0.454;
 	System.out.print( pounds + "pounds is" + kilograms + "kilograms")
