938b3d30a8a1dee0487125b5a7cb0649539e04e8
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-22 23:07:43 +0800

    Update 2-9.java

--- 2-9.java
+++ 2-9.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
-	public class Acceleration {
-		public static void main(String[] args) {
+public class Acceleration {
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			Scanner input = new Scanner(System.in);
-			System.out.print("Enter v0,v1,and t:");
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter v0,v1,and t: ");
 			double v0 = input.nextDouble();
 			double v1 = input.nextDouble();
 			double t = input.nextDouble();
-			double averageAcceleration = ( v1 - v0 )/t;
-			System.out.println("The average acceleration is"+averageAcceleration);
+			double averageAcceleration = ( v1 - v0 ) / t;
+			System.out.println("The average acceleration is" + averageAcceleration);
 		}
 
 	}
