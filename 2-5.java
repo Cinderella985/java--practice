e137b95ec23e0c71494f1a1bd4dbd46b5f52ab2c
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-22 22:42:37 +0800

    Update 2-5.java

--- 2-5.java
+++ 2-5.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter the subtotal and a gratuity");
-		double  subtotal = input.nextDouble();
-		double  gratuityRate  = input.nextDouble();
-		double  gratuity = gratuityRate/100 *subtotal;
-		double  total = subtotal + gratuity;
-		System.out.print("The gratuity is $ "+gratuity+"and total is $"+total);
+		double subtotal = input.nextDouble();
+		double gratuityRate = input.nextDouble();
+		double gratuity = gratuityRate / 100 * subtotal;
+		double total = subtotal + gratuity;
+		System.out.print("The gratuity is $ " + gratuity + " and total is $ " + total);
 	
 		}
  }
