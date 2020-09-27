2759b139a5291287195bb2ebb918a9a57ecddd0b
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-27 19:35:24 +0800

    fix 3-15.java
    更改了优先顺序，随机数的产生。

--- 3-15.java
+++ 3-15.java
@@ -8,13 +8,13 @@ public class Op {
 		
 		System.out.print("Enter a three-digit integer: ");
 		int number = input.nextInt();
-		int lottery = (int)(Math.random() * 10);
+		int lottery = (int)(Math.random() * 100);
 		
 		if (number == lottery)
 			System.out.print("you get $10000");
-		else if (number / 100 == lottery % 10 || number / 100 == lottery /100 || number / 100 == lottery/ 10 % 10 && number % 10 == lottery % 10 || number % 10 == lottery /100 || number % 10 == lottery/ 10 % 10 && number / 10 % 10 == lottery % 10 || number / 10 % 10 == lottery /100 || number / 10 % 10 == lottery/ 10 % 10 )
+		else if ((number / 100 == lottery % 10 || number / 100 == lottery /100 || number / 100 == lottery/ 10 % 10) && (number % 10 == lottery % 10 || number % 10 == lottery /100 || number % 10 == lottery/ 10 % 10) &&(number / 10 % 10 == lottery % 10 || number / 10 % 10 == lottery /100 || number / 10 % 10 == lottery/ 10 % 10 ))
 			System.out.print("you get $3000");
-		else if ((number / 100 == lottery % 10 || number / 100 == lottery /100 || number / 100 == lottery/ 10 % 10) ||( number % 10 == lottery % 10 || number % 10 == lottery /100 || number % 10 == lottery/ 10 % 10) || (number / 10 % 10 == lottery % 10 || number / 10 % 10 == lottery /100 || number / 10 % 10 == lottery/ 10 % 10))
+		else if ((number / 100 == lottery % 10) ||( number / 100 == lottery /100 )|| (number / 100 == lottery/ 10 % 10) ||( number % 10 == lottery % 10) || (number % 10 == lottery /100) || (number % 10 == lottery/ 10 % 10) || (number / 10 % 10 == lottery % 10 )||( number / 10 % 10 == lottery /100) ||( number / 10 % 10 == lottery/ 10 % 10))
 			System.out.print("you get $1000");
 		else
 			System.out.print("you get nothing");
