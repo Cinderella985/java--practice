605b61cf86cbcbbd730556a5d2f6ebdae8630e55
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-27 20:31:06 +0800

    fix 3-9
    根据题目要求修改输入

--- 3-9.java
+++ 3-9.java
@@ -6,19 +6,20 @@ public class Ds {
 		//Create a Scanner
 		Scanner input = new Scanner(System.in);
 		
-		System.out.print("Enter d1, d2, d3, d4, d5, d6, d7, d8, d9,: ");
-		int d1 = input.nextInt();
-		int d2 = input.nextInt();
-		int d3 = input.nextInt();
-		int d4 = input.nextInt();
-		int d5 = input.nextInt();
-		int d6 = input.nextInt();
-		int d7 = input.nextInt();
-		int d8 = input.nextInt();
-		int d9 = input.nextInt();
+		System.out.print("Enter the first 9 digits of an ISBN as integer : ");
+		int x = input.nextInt();
+		int d1 = x / 100000000;
+		int d2 = x / 10000000;
+		int d3 = x / 1000000 % 10;
+		int d4 = x / 100000 % 10;
+		int d5 = x / 10000 % 10;
+		int d6 = x / 1000 % 10;
+		int d7 = x / 100 % 10;
+		int d8 = x / 10 % 10;
+		int d9 = x % 10;
 		int d10 = (d1 * 1 + d2 * 2 + d3 * 3 + d4 * 4 + d5 * 5 + d6 * 6 + d7 * 7 + d8 * 8 + d9 * 9)
  % 11;	
-	
+		System.out.print(d3);
 		if (d10 == 10)
 			System.out.print("The ISBN-10 number is" + d1+""+d2+""+d3+""+d4+""+d5+""+d6+""+d7+""+d8+""+d9+"X");
 		else{
