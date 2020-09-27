616cf85bbe7d5aff5014c105569f76464d190540
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-27 20:32:12 +0800

    fix 3-9(2)
    删除不该有的内容

--- 3-9.java
+++ 3-9.java
@@ -19,7 +19,7 @@ public class Ds {
 		int d9 = x % 10;
 		int d10 = (d1 * 1 + d2 * 2 + d3 * 3 + d4 * 4 + d5 * 5 + d6 * 6 + d7 * 7 + d8 * 8 + d9 * 9)
  % 11;	
-		System.out.print(d3);
+		
 		if (d10 == 10)
 			System.out.print("The ISBN-10 number is" + d1+""+d2+""+d3+""+d4+""+d5+""+d6+""+d7+""+d8+""+d9+"X");
 		else{
