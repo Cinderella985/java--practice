e8928a4362a56f0d0af44eceee583de266104152
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-28 17:24:49 +0800

    fix(2) 3-11

--- 3-11.java
+++ 3-11.java
@@ -26,13 +26,13 @@ public class Ki {
 		case 12: System.out.print("December");break;
 		}
 		if (month == 2 && yes == true){
-			System.out.print("" + year + "has 29 days");}
+			System.out.print(" " + year + "has 29 days");}
 		else if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-			System.out.print("" + year + "has 31 days");	
+			System.out.print(" " + year + "has 31 days");	
 		else if (month == 2 && yes != true)
-			System.out.print("" + year + "has 28 days");
+			System.out.print(" " + year + "has 28 days");
 		else
-			System.out.print("" + year + "has 30 days");	
+			System.out.print(" " + year + "has 30 days");	
 		}
 		
 		
