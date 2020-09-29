202ae6288e4ba6dc22c21bd2f1ce3d455d5e9cc4
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-29 10:45:21 +0800

    fixed 3-24

--- 3-24.java
+++ 3-24.java
@@ -2,28 +2,27 @@ import java.util.Scanner;
 
 public class Card {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int number1 = 1 + (int)(Math.random() * 13);
 		int number2 = (int)(Math.random() * 4);
 	
 		if (number1 == 1 )
-			System.out.print("The card you picked is Ace  ");
+			System.out.print("The card you picked is Ace ");
 		else if (number1 == 11 )
 			System.out.print("The card you picked is Jack ");
 		else if (number1 == 12 )
-			System.out.print("The card you picked is Queen");
-		else if (number1 == 12 )
-			System.out.print("The card you picked is King");
+			System.out.print("The card you picked is Queen ");
+		else if (number1 == 13 )
+			System.out.print("The card you picked is King ");
 		else
-			System.out.print("The card you picked is " + number1);
+			System.out.print("The card you picked is " + number1 );
 						
 		if (number2 == 0)
-			System.out.print("of Clubs");
+			System.out.print(" of Clubs ");
 		else if (number2 == 1 )
-			System.out.print("of Diamonds ");
+			System.out.print(" of Diamonds ");
 		else if (number2 == 2 )
-			System.out.print("of Hearts");
+			System.out.print(" of Hearts ");
 		else
-			System.out.print("of Spades");
+			System.out.print(" of Spades ");
 	}
 }
