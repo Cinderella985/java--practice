446fe2285524fece45818968bc76031d1edb39a0
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-29 18:30:54 +0800

    fixed 3-18

--- 3-18.java
+++ 3-18.java
@@ -2,26 +2,22 @@ import java.util.Scanner;
 
 public class Profit {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//Create a Scanner
 		Scanner input = new Scanner(System.in);
-		
 		System.out.print("Enter a weight of package: ");
-		int w = input.nextInt();
+		double w = input.nextDouble();
 		
 		if (0 < w && w <= 1)
-			System.out.print("3.5");
-		else if (1 < w && w <= 3)
-			System.out.print("5.5");
-		else if (3 < w && w <= 10)
-			System.out.print("8.5");
-		else if (10 < w && w <= 20)
-			System.out.print("10.5");
-		else if (10 < w && w <= 20)
-			System.out.print("10.5");
+			System.out.print("The shipping cost is $3.5");
+		else if (w <= 3)
+			System.out.print("The shipping cost is $5.5");
+		else if (w <= 10)
+			System.out.print("The shipping cost is $8.5");
+		else if (w <= 20)
+			System.out.print("The shipping cost is $10.5");
 		else if (w > 20)
 			System.out.print("the package cannot be shipped");
-		else if (w <= 0)
+		else
 			System.out.print("Invaild input");
 	}
 }
