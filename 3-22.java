212bd1a6a2b9c2cce0964caeed9abf6aa5fd7136
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-29 10:35:54 +0800

    fixed 3-22

--- 3-22.java
+++ 3-22.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Circle {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//Create a Scanner
 		Scanner input = new Scanner(System.in);
 		
@@ -12,10 +11,8 @@ public class Circle {
 		
 		if (Math.pow(number1 * number1 + number2 * number2, 0.5) <= 10 )
 			System.out.print("Point (" + number1 + "," + number2 + ") is in the circle");
-		if (Math.pow(number1 * number1 + number2 * number2, 0.5) > 10 )
+		else
 			System.out.print("Point (" + number1 + "," + number2 + ") is not in the circle");
 	}
 }	
-		
-		
 		
