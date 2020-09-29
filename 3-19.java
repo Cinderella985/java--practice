aa92521112bfd4ca45b7dfb621cb58938d6a9b91
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-29 19:01:45 +0800

    fixed 3-19

--- 3-19.java
+++ 3-19.java
@@ -2,17 +2,16 @@ import java.util.Scanner;
 
 public class Triangle {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//Create a Scanner
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter the three numbers of triangle : ");
-		int a = input.nextInt();
-		int b = input.nextInt();
-		int c = input.nextInt();
+		double a = input.nextDouble();
+		double b = input.nextDouble();
+		double c = input.nextDouble();
 		
 		if ((a + b > c) && (a + c > b) && (b + c > a))
-			System.out.print(a + b + c);
+			System.out.print("The perimeter is" + (a + b + c));
 		else
 			System.out.print("Invalid input");
 	}
