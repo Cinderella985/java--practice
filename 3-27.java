6fd4eb5164262c3e426dcb70557d38027b2c694f
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-29 10:51:20 +0800

    fixed 3-27

--- 3-27.java
+++ 3-27.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Dadlic {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//Create a Scanner
 		Scanner input = new Scanner(System.in);
 		
@@ -10,9 +9,9 @@ public class Dadlic {
 		double number1 = input.nextDouble();
 		double number2 = input.nextDouble();
 		
-		if (number1 < 200 - 2 * number2 && number2 < 100 - 0.5 * number1)
+		if (number1 <= 200 - 2 * number2 && number2 <= 100 - 0.5 * number1)
 			System.out.print("The point is in the triangle");
-		if (number1 >= 200 - 2 * number2 && number2 >= 100 - 0.5 * number1)
+		else
 			System.out.print("The point is not in the triangle");
 	}
 }
