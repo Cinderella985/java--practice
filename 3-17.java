749a9beeb3ef63cdffff8c3d034b806db246143e
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-29 18:53:51 +0800

    fixed 3-17

--- 3-17.java
+++ 3-17.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 
 public class Lo {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int b = (int)(Math.random() * 3);
-		
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter scissor (0), rock (1), paper (2) : ");
 		int a = input.nextInt();
+		
+		int b = (int)(Math.random() * 3);
 		
 		if (a == 0 && b == 1)
 			System.out.print("You won");
@@ -26,7 +25,7 @@ public class Lo {
 			System.out.print("You win");	
 		else if (a == 2 && b == 0)
 			System.out.print("You lose");	
-		else if (a == 2 && b == 2)
+		else
 			System.out.print("It is a draw");
 	}
 }
