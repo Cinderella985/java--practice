a062781dcf4000559330c93cbdd31e788492f147
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-29 18:42:05 +0800

    fixed 3-16

--- 3-16.java
+++ 3-16.java
@@ -1,8 +1,5 @@
-import java.util.Scanner;
-
 public class Recetangle {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int number1 = (int)(Math.random() * 100);
 		int number2 = (int)(Math.random() * 200);
 		System.out.print("(" + number1 + "," + number2 + ")");
