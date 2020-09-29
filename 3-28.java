7973e5bf0ecfabacea01d15f4c29c9c6fa1af5b1
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-29 18:21:40 +0800

    fixed 3-28

--- 3-28.java
+++ 3-28.java
@@ -7,19 +7,23 @@ public class Two {
 		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter a r1's center x- and y-coordinates, width, and height : ");
-		System.out.print("Enter a r2's center x- and y-coordinates, width, and height : ");
 		double x1 = input.nextDouble();
 		double y1 = input.nextDouble();
 		double w1 = input.nextDouble();
 		double h1 = input.nextDouble();
+		
+		System.out.print("Enter a r2's center x- and y-coordinates, width, and height : ");
 		double x2 = input.nextDouble();
 		double y2 = input.nextDouble();
 		double w2 = input.nextDouble();
 		double h2 = input.nextDouble();
 		
-		if ((x2 - x1) < 0.5 * w1 && (y2 - y1) < 0.5 * h1)
+		double xDistance = x1 >= x2 ? x1 - x2 : x2 - x1 ;
+		double yDistance = y1 >= y2 ? y1 - y2 : y2 - y1 ;
+		
+		if (xDistance < 0.5 * w1 && yDistance < 0.5 * h1)
 			System.out.print("r2 is inside r1");
-		else if ((x2 - x1) < 0.5 * w1 + 0.5 * w2 && (y2 - y1) < 0.5 * h1 + 0.5 * h2)
+		else if (xDistance <= 0.5 * w1 + 0.5 * w2 && yDistance <= 0.5 * h1 + 0.5 * h2)
 			System.out.print("r2 overlaps r1");
 		else
 			System.out.print("r2 does not overlaps r1");
