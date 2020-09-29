aa991550c2a982150fcec8657613e5ba39bf3935
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-29 10:29:46 +0800

    fixed 3-22

--- 3-23.java
+++ 3-23.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Square {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		//Create a Scanner
 		Scanner input = new Scanner(System.in);
 		
@@ -10,12 +9,11 @@ public class Square {
 		double number1 = input.nextDouble();
 		double number2 = input.nextDouble();
 		
-		if ( number1 < 5 &&  number2 < 2.5 )
+		if ( number1 - 0 <= 5 && number2 - 0 <= 2.5 )
 			System.out.print("Point (" + number1 + "," + number2 + ") is in the rectangle");
-		if ( number1 > 5 &&  number2 > 2.5 )
+		else
 			System.out.print("Point (" + number1 + "," + number2 + ") is not in the rectangle");
 	}
 }	
-		
 		
 		
