576f491c81136123debbc14b48df44647d265094
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-09-30 18:58:35 +0800

    fixed (2) 3-27

--- 3-27.java
+++ 3-27.java
@@ -9,7 +9,7 @@ public class Dadlic {
 		double number1 = input.nextDouble();
 		double number2 = input.nextDouble();
 		
-		if (number1 <= 200 - 2 * number2 && number2 <= 100 - 0.5 * number1)
+		if (number1 <= 200 - 2 * number2 && number1 >= 0 && number2 <= 100 - 0.5 * number1 && number2 >= 0)
 			System.out.print("The point is in the triangle");
 		else
 			System.out.print("The point is not in the triangle");
