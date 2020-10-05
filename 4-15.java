ecfdc0fd24f3b398147f711a414f1a8f33dd0a05
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-10-05 20:48:31 +0800

    fixed 4-15

--- 4-15.java
+++ 4-15.java
@@ -7,95 +7,33 @@ public class Telephone {
 		System.out.print("Enter a letter: ");
 		char x1 = input.next().toLowerCase().charAt(0);
 		
-		if (x1 == 'a') {
+		if (x1 == 'a' || x1 == 'b' || x1 == 'c') {
 			System.out.print("The corresponding number is 2");
 		}
-		else if (x1 == 'b') {
-			System.out.print("The corresponding number is 2");
-		}
-		else if (x1 == 'c') {
-			System.out.print("The corresponding number is 2");
-		}
-		else if (x1 == 'd') {
+		else if (x1 == 'd' || x1 == 'e' || x1 == 'f') {
 			System.out.print("The corresponding number is 3");
 		}
-		else if (x1 == 'e') {
-			System.out.print("The corresponding number is 3");
-		}
-		else if (x1 == 'f') {
-			System.out.print("The corresponding number is 3");
-		}
-		else if (x1 == 'g') {
+		else if (x1 == 'g' || x1 == 'h' || x1 == 'i') {
 			System.out.print("The corresponding number is 4");
 		}
-		else if (x1 == 'h') {
-			System.out.print("The corresponding number is 4");
-		}
-		else if (x1 == 'i') {
-			System.out.print("The corresponding number is 4");
-		}
-		else if (x1 == 'j') {
+		else if (x1 == 'j' || x1 == 'k' || x1 == 'l') {
 			System.out.print("The corresponding number is 5");
 		}
-		else if (x1 == 'k') {
-			System.out.print("The corresponding number is 5");
-		}
-		else if (x1 == 'l') {
-			System.out.print("The corresponding number is 5");
-		}
-		else if (x1 == 'm') {
+		else if (x1 == 'm' || x1 == 'n' || x1 == 'o') {
 			System.out.print("The corresponding number is 6");
 		}
-		else if (x1 == 'n') {
-			System.out.print("The corresponding number is 6");
-		}
-		else if (x1 == 'o') {
-			System.out.print("The corresponding number is 6");
-		}
-		else if (x1 == 'p') {
+		else if (x1 == 'p' || x1 == 'q' || x1 == 'r' || x1 == 's') {
 			System.out.print("The corresponding number is 7");
 		}
-		else if (x1 == 'q') {
-			System.out.print("The corresponding number is 7");
-		}
-		else if (x1 == 'r') {
-			System.out.print("The corresponding number is 7");
-		}
-		else if (x1 == 's') {
-			System.out.print("The corresponding number is 7");
-		}
-		else if (x1 == 't') {
+		else if (x1 == 't' || x1 == 'u' || x1 == 'v') {
 			System.out.print("The corresponding number is 8");
 		}
-		else if (x1 == 'u') {
-			System.out.print("The corresponding number is 8");
-		}
-		else if (x1 == 'v') {
-			System.out.print("The corresponding number is 8");
-		}
-		else if (x1 == 'w') {
-			System.out.print("The corresponding number is 9");
-		}
-		else if (x1 == 'x') {
-			System.out.print("The corresponding number is 9");
-		}
-		else if (x1 == 'y') {
-			System.out.print("The corresponding number is 9");
-		}
-		else if (x1 == 'z') {
+		else if (x1 == 'w' || x1 == 'x' || x1 == 'y' || x1 == 'z') {
 			System.out.print("The corresponding number is 9");
 		}
 		else {
-			System.out.print(x1 + " is an invalid number");
+			System.out.print(x1 + " is an invalid input");
 		}
 	}
 }
-		
-		
-		
-		
-		
-		
-		
-		
 		
