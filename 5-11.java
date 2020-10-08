de6fd4d133be5cd02ad5f052e6dc9966a6f262a9
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-10-08 23:03:25 +0800

    Update 5-11.java

--- 5-11.java
+++ 5-11.java
@@ -1,14 +1,14 @@
 public class H {
 	public static void main(String[] args) {
 		final int NUMBER_PER_LINE = 10;
-		int count =0;
+		int count = 0;
 		
 		System.out.println("The numbers from 100 to 200 are ");
 		
 		for (int number = 100; number <= 200; number++) {
-			if ((number % 5 == 0 && number % 6 != 0) || (number % 5 != 0 && number % 6 ==0)) {
+			if ((number % 5 == 0 && number % 6 != 0) || (number % 5 != 0 && number % 6 == 0)) {
 				count++;
-			if (count % NUMBER_PER_LINE ==0) 
+			if (count % NUMBER_PER_LINE == 0) 
 				System.out.println(number);
 			else 
 				System.out.print(number + " ");	
