236d52ba7bd3cf17d2819bd10871c771fe595846
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-10-09 14:52:51 +0800

    fixed 5-14

--- 5-14.java
+++ 5-14.java
@@ -2,13 +2,16 @@ import java.util.Scanner;
 
 public class I {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in) ;
+		Scanner input = new Scanner(System.in);
 		
 		System.out.print("Enter first integer: ");
 		int n1 = input.nextInt();
 		
 		System.out.print("Enter second integer: ");
 		int n2 = input.nextInt();
+		
+		n1 = (n1 > 0) ? n1 : -n1;
+		n2 = (n2 > 0) ? n2 : -n2;
 		
 		int temp,gcd = -1;
 		
@@ -21,10 +24,9 @@ public class I {
 		while (temp >= 1) {
 			if (n1 % temp == 0 && n2 % temp == 0 && gcd < temp) {
 				gcd = temp;	
-			System.out.print("The greastest common divisior for " + n1 + " and " + n2 + " is " + gcd);
+				System.out.print("The greastest common divisior is " + gcd);
 			}
 			temp--;
-		}
+		}	
 	}
 }
-		 
