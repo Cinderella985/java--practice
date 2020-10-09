edba45eb60d8c51c99b83341795e5cbfbac5d2bb
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-10-09 14:21:18 +0800

    fixed 5-8

--- 5-8.java
+++ 5-8.java
@@ -8,7 +8,7 @@ public class F {
 		int number1 = input.nextInt();
 			
 		System.out.print("Enter the scores of students: ");
-		int number2 ,max;
+		int number2 ,max,number3 = 0;
 		number2 = input.nextInt();
 		max = number2;
 			
@@ -17,17 +17,19 @@ public class F {
 		name1 = input.next();
 		maxname = name1;
 			
-		while (number2 != 0) {
+		while (number3 < number1 - 1) {
 			System.out.print("Enter the scores of students: ");
 			number2 = input.nextInt();
 			
 			System.out.print("Enter the name of students: ");
 			name1 = input.next();
 			
+			number3++;
+			
 			if (number2 > max) {
 				max = number2;
 				maxname = name1; 
-			}		
+			}	
 		}
 		
 		System.out.print(maxname + " gets the highest scores");
