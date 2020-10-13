9d68c238e4794037dedd690975e01865040135fa
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-10-13 16:17:23 +0800

    fixed 5-18(2)

--- 5-18（2）.java
+++ 5-18（2）.java
@@ -1,11 +1,11 @@
-public class N {
+class Exe5_182 {
 	public static void main(String[] args) {
 		int number = 6;
 		
 		for (int row = number; row >= 1; row--) {
 			for (int column = 1; column <= row; column++)
 				System.out.print(column + " ");
-				System.out.print("\n");
+			System.out.print("\n");
 		}
 	}
 }
