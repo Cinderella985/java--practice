4dd51ee3ac12264f24b3b964981f36044c12ccd8
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-10-17 18:03:48 +0800

    fixed 5-33

--- Exe5_23.java
+++ Exe5_23.java
@@ -1,12 +1,12 @@
-class Exe5__23 {
+class Exe5_23 {
 	public static void main(String[] args) {
 	
 		for (int a = 1; a <= 10000; a++) {
 			int sum = 0;
 			for (int b = 1; b <= a / 2; b++) {
-			if (a % b == 0)
-				sum += b;
-		}
+				if (a % b == 0)
+					sum += b;
+			}
 			if (sum == a) {
 				System.out.print(" " + a);
 			}
