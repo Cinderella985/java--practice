d03b2336a8783613ea377c6c0d46536875f75c47
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-10-17 18:06:10 +0800

    fixed 5-41

--- Exe5_41.java
+++ Exe5_41.java
@@ -8,17 +8,16 @@ class Exe5_41 {
 		int x1 = input.nextInt();
 		
 		int max = x1;
-		int count = 0;
+		int count = 1;
 		
 		while (x1 != 0) {
 			x1 = input.nextInt();
 			if (x1 > max) {
 				max = x1;
-				count = 0;
+				count = 1;
 			}
-			if (x1 == max) {
+			else if (x1 == max) 
 				count++;
-			}
 		}
 		System.out.println(max);
 		System.out.println(count);
