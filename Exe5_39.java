faa87bd58fb50c16ec2a1d9c1cfacca75eafb16c
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-10-17 18:05:23 +0800

    fixed 5-39

--- Exe5_39.java
+++ Exe5_39.java
@@ -1,14 +1,15 @@
 class Exe5_39 {
 	public static void main(String[] args) {
 		
+		final  double x0 = 5000;
 		double x1 = 5000 * 0.08;
 		double x2 = 5000 * 0.1;
 		double x3 = x1 + x2;
 		double x4 = 10000;
 		
-		while ((x4 - 10000) * 0.12 + x3 <= 30000) {
-			x4++;
+		while ((x4 - 10000) * 0.12 + x3 + x0 < 30000) {
+			x4 += 0.01;
 		}
-		System.out.print(x4 + 10000);	
+		System.out.printf("%.2f",(x4 + 10000));	
 	}
 }
