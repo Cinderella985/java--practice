ca35a8b0e2c2666a816d16562d17c9e638eaa873
Cinderella985 <71483754+Cinderella985@users.noreply.github.com>
2020-10-17 18:04:42 +0800

    fixed 5-37

--- Exe5_37.java
+++ Exe5_37.java
@@ -1,28 +1,19 @@
 import java.util.Scanner;
 
-public class Exe5_37 {
+class Exe5_37 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
 		System.out.print("Enter an integer in decimal system: ");
 		int x1 = input.nextInt();
 		
-		int x3 = 0;
-		int x2;
+		String x2 = "";
 		
-		while (x3 <= x1) {
-			if (x3 <= 3)	
-				x3 += 1;
-			x2 = x3 % 2;
-			System.out.print(x2 + " ");
-			x3 = x3 * 2;
+		while (x1 != 0) {
+			x2 = x1 % 2 + x2;
+			x1 = x1 / 2;
 		}
-	}	
+		System.out.print(x2);
+	}
 }
-				
 		
-		
-		
-		
-		
-
